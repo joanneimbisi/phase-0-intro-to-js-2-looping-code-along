6198e6988a1201ac11f94fea432b9d9a0af71912
Joanne Imbisi <evizengwa@yahoo.com>
2022-06-12 17:47:08 -0700

    commit tree 377f42c0c21469221115a67ade9c7f60368c2965

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["James", "Charles", "Chloe"]
+
+function writeCards(names, event) {
+    let thankYouMessages= []
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return thankYouMessages
+}
+
+
+function countDown(firstNumber) {
+    while (firstNumber>0){
+        console.log(firstNumber);
+        firstNumber -= 1;
+    }
+    console.log(firstNumber);
+}
